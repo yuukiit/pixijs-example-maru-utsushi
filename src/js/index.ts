cbffce39ec5d88ad27f613c032a969665691e2d1
yuuki takahashi <contact.me@in-colors.net>
2021-02-20 22:25:12 +0900

    [Add] blank files.

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,9 +1,19 @@
 import container from './demos-basic/container'
 import transparentBackground from './demos-basic/transparent-background'
+import tinting from './demos-basic/tinting'
+// import cacheAsBitmap from './demos-basic/cache-as-bitmap'
+// import particleContainer from './demos-basic/particle-container'
+// import blendModes from './demos-basic/blend-modes'
+// import simplePlane from './demos-basic/simple-plane'
 
 const pixiFunctions = {
   container: () => container(),
   transparentBackground: () => transparentBackground(),
+  tinting: () => tinting(),
+  // cacheAsBitmap: () => cacheAsBitmap(),
+  // particleContainer: () => particleContainer(),
+  // blendModes: () => blendModes(),
+  // simplePlane: () => simplePlane(),
 }
 
 window.addEventListener('load', () => {
