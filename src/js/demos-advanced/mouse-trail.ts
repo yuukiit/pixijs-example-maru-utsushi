f348fe88d8b645a8ef9f90bbe4c17791ec41c92c
yuuki takahashi <contact.me@in-colors.net>
2021-03-07 18:47:13 +0900

    [Add] mouse-trail, scratchcard

--- src/js/demos-advanced/mouse-trail.ts
+++ src/js/demos-advanced/mouse-trail.ts
@@ -1,4 +1,69 @@
 import * as PIXI from 'pixi.js'
-import { Dude } from '../../../types/dude';
 
-export default () => {}
+export default () => {
+  const app = new PIXI.Application({ backgroundColor: 0x1099bb });
+  document.body.appendChild(app.view);
+
+  const trailTexture = PIXI.Texture.from('/assets/img/ika.jpg');
+  const historyX: number[] = [];
+  const historyY: number[] = [];
+  const historySize: number = 20;
+  const ropeSize: number = 100;
+  const points: PIXI.Point[] = [];
+
+  // Create history array.
+  for (let index = 0; index < historySize; index++) {
+    historyX.push(0);
+    historyY.push(0);
+  }
+
+  // Create rope points.
+  for (let index = 0; index < ropeSize; index++) {
+    points.push(new PIXI.Point(0, 0));
+  }
+
+  // Create the rope.
+  const rope = new PIXI.SimpleRope(trailTexture, points);
+  // rope.blendMode = PIXI.BLEND_MODES.ADD;
+  app.stage.addChild(rope);
+
+  /**
+   * Cubic interpolation based on https://github.com/osuushi/Smooth.js
+   */
+  const clipInput = (k: number, arr: number[]) => {
+    if (k < 0) k = 0;
+    if (k > arr.length - 1) k = arr.length - 1;
+    return arr[k];
+  }
+
+  const getTangent = (k: number, factor: number, array: number[]) => {
+    return factor * (clipInput(k + 1, array) - clipInput(k - 1, array)) / 2;
+  }
+
+  const cubicInterpolation = (array: number[], t: number, tangentFactor?: null | number): number => {
+    if (tangentFactor == null) tangentFactor = 1;
+    const k = Math.floor(t);
+    const m = [getTangent(k, tangentFactor, array), getTangent(k + 1, tangentFactor, array)];    const p = [clipInput(k, array), clipInput(k + 1, array)];
+    t -= k;
+    const t2 = t * t;
+    const t3 = t * t2;
+    return (2 * t3 - 3 * t2 + 1) * p[0] + (t3 - 2 * t2 + t) * m[0] + (-2 * t3 + 3 * t2) * p[1] + (t3 - t2) * m[1];
+  }
+
+  app.ticker.add((_delta) => {
+    const mouseposition = app.renderer.plugins.interaction.mouse.global;
+    historyX.pop();
+    historyX.unshift(mouseposition.x);
+    historyY.pop();
+    historyY.unshift(mouseposition.y);
+    for (let index = 0; index < ropeSize; index++) {
+      const p = points[index];
+      const ix = cubicInterpolation(historyX, index / ropeSize * historySize);
+      const iy = cubicInterpolation(historyY, index / ropeSize * historySize);
+
+      p.x = ix;
+      p.y = iy;
+    }
+
+  })
+}
--- src/js/demos-advanced/scratchcard.ts
+++ src/js/demos-advanced/scratchcard.ts
@@ -1,4 +1,60 @@
 import * as PIXI from 'pixi.js'
-import { Dude } from '../../../types/dude';
 
-export default () => {}
+export default () => {
+  const app = new PIXI.Application();
+  document.body.appendChild(app.view);
+  const { stage } = app;
+
+  const brush = new PIXI.Graphics();
+  brush.beginFill(0xffffff);
+  brush.drawCircle(0, 0, 40);
+  brush.endFill();
+
+  const setup = (
+    loader: PIXI.Loader,
+    resources: Partial<Record<string, PIXI.LoaderResource>>
+  ) => {
+    const background = new PIXI.Sprite(resources.t1.texture);
+    stage.addChild(background);
+    background.width = app.screen.width;
+    background.height = app.screen.height;
+
+    const imageToReveal = new PIXI.Sprite(resources.t2.texture);
+    stage.addChild(imageToReveal);
+    imageToReveal.width = app.screen.width;
+    imageToReveal.height = app.screen.height;
+
+    const renderTexture = PIXI.RenderTexture.create({
+      width: app.screen.width,
+      height: app.screen.height
+    });
+    const renderTextureSprite = new PIXI.Sprite(renderTexture);
+    stage.addChild(renderTextureSprite);
+    imageToReveal.mask = renderTextureSprite;
+
+    let dragging = false;
+    const pointerDown = (event: PIXI.InteractionEvent) => {
+      dragging = true;
+      pointerMove(event);
+    }
+    const pointerUp = (_event: PIXI.InteractionEvent) => {
+      dragging = false;
+    }
+    const pointerMove = (event: PIXI.InteractionEvent) => {
+      if (dragging) {
+        console.log(event.data.global);
+        brush.position.copyFrom(event.data.global);
+        app.renderer.render(brush, renderTexture, false, null, false);
+      }
+    }
+
+    app.stage.interactive = true;
+    app.stage.on('pointerdown', pointerDown);
+    app.stage.on('pointerup', pointerUp);
+    app.stage.on('pointermove', pointerMove);
+  }
+
+  app.loader.add('t1', '/assets/img/nakatoro.png');
+  app.loader.add('t2', '/assets/img/ika.jpg');
+  app.loader.load(setup);
+}
